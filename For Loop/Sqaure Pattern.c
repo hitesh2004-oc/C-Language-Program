ed319ba21d07f4c37ea759ce15d684a81114fbba
Hitesh Malviya <hiteshmalviya955@gmail.com>
2025-09-27 19:57:23 +0530

    C Language Program

---
+++ For Loop/Sqaure Pattern.c
@@ -0,0 +1,309 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 4;
+   for(int i = 1 ; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }      
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        printf("\n");
+    }
+    for(int i = 2 ; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }      
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        printf("\n");
+    }
+    for(int i = 2 ; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }      
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        printf("\n");
+    }
+    for(int i = 2 ; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }      
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        printf("\n");
+    }
+    for(int i = 2 ; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }      
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        printf("\n");
+    }
+    for(int i = 2 ; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }      
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        printf("\n");
+    }
+    for(int i = 2 ; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }      
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        for(int j = 2; j <= n; j++){
+            if(j == n || j == 1 || i == n || i == 1) printf("* ");
+            else printf("  ");
+
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
